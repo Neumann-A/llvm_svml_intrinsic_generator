2448f63c006a6ad6bacbce48c75d54d6f1b80606
Alexander Neumann <alexander.neumann@hamburg.de>
2019-04-26 13:59:06 +0200

    added correct inline assembly.

--- SVML_Test.cpp
+++ SVML_Test.cpp
@@ -37,8 +37,8 @@ static_assert(sizeof(Vectorf8) == sizeof(__m256));
 //extern __cdecl __m256d (&__vdecl_sincos4(__m256d))[2];
 extern "C"
 {
-#define __DEFAULT_FN_ATTRS __attribute__((__always_inline__, __nodebug__,ms_abi, __target__("avx"), __min_vector_width__(256)))
-#define __DEFAULT_FN_ATTRS_CALLER __attribute__((__always_inline__, __nodebug__,no_caller_saved_registers,ms_abi, __target__("avx"), __min_vector_width__(256)))
+#define __DEFAULT_FN_ATTRS __attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(256)))
+#define __DEFAULT_FN_ATTRS_CALLER __attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(256)))
 #define __DEFAULT_FN_ATTRS128 __attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(128)))
 #define __NO_CALLER_SAVED_REGISTER__ __attribute__((no_caller_saved_registers))
 #define __REPARAM__ __attribute__((regparm(2)))
@@ -54,24 +54,60 @@ extern "C"
 		return __vdecl_cos4(val);
 	}
 
-	extern __m256d
-		__attribute__((__always_inline__, __nodebug__,  __target__("avx"), __min_vector_width__(256)))
-		 __vdecl_sincos4(); //The return value is nessecary so that clang does not insert a vzeroupper before the call!
+	//extern __m256d
+	//	__attribute__((__always_inline__, __nodebug__,  __target__("avx"), __min_vector_width__(256)))
+	//	 __vdecl_sincos4(); //The return value is nessecary so that clang does not insert a vzeroupper before the call!
 
-	static __m256d
-		__attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(256)))
-		_mm256_sincos_pd(__m256d* pcosres, __m256d val)
+	//extern __inline__ __m256d _mm256_sincos_pd(__m256d*, __m256d);
+
+	static __inline__ __m256d __DEFAULT_FN_ATTRS _mm256_sincos_pd(__m256d* pcosres, __m256d input)
 	{
-		__asm vmovapd ymm0, ymmword ptr[r8]; // Write val[r8] into ymm0
-		__vdecl_sincos4(); // Changes RAX, RCX & RDX; Defined as extern __m256d __vdecl_sincos4(); 
-		__m256d& cos = *pcosres; // Generates a required move instruction before the function call
-		__asm vmovapd ymmword ptr[cos], ymm1; //Write cosine result
+		__asm__(".intel_syntax noprefix");
+		__asm__("sub $32, %rsp"); 
+		__asm__("call __vdecl_sincos4 \t\n"
+			: [sin] "+v" (input), [cos] "=v" (*pcosres)
+			:
+			: "%rax", "%rcx", "%rdx"
+			);
+		__asm__("add $32, %rsp");
+		return input; // Input will be ymm0 and will be changed correctly
+	};
+	//extern void __vdecl_sincos4();
+	//__m256d __declspec(noinline)
+	//	__attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(256)))
+	//	_mm256_sincos_pd(__m256d*, __m256d)
+	//{
+	//	__asm {
+	//		//push rdi; preserve rdi;
+	//		//push rsi; preserve rsi
+	//		sub rsp, 020h; allocate stack space
+	//		mov rdi, rcx; store sine result adress in rdi;
+	//		mov rsi, rdx; store cosine result adress in rdi;
+	//		vmovapd ymm0, ymmword ptr[r8]; Move value to calculate from into reg
+	//		call __vdecl_sincos4; call the vdecl symbol
+	//		vmovapd ymmword ptr[rdi], ymm1; store cosine result
+	//		vmovapd ymmword ptr[rsi], ymm0; store cosine result
+	//		add rsp, 020h; deallocate stack space
+	//		//pop rsi; restore rsi;
+	//		//pop rdi; restore rdi;
+	//		//ret; sine result is returned in ymm0
+	//	}
+	//};
+	//static __declspec(noinline) __m256d
+	//	__attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(256)))
+	//	_mm256_sincos_pd(__m256d* pcosres, __m256d val)
+	//{
+	//	__asm vmovapd ymm0, ymmword ptr[r8]; // Write val[r8] into ymm0
+	//	__vdecl_sincos4(); // Changes RAX, RCX & RDX; Defined as extern __m256d __vdecl_sincos4(); 
+	//	//__asm call __vdecl_sincos4;
+	//	__m256d& cos = *pcosres; // Generates a required move instruction before the function call
+	//	__asm vmovapd ymmword ptr[cos], ymm1; //Write cosine result
 
-		__m256d sin;
-		__asm vmovapd ymmword ptr[sin], ymm0; //Write sine result into sin
+	//	__m256d sin;
+	//	__asm vmovapd ymmword ptr[sin], ymm0; //Write sine result into sin
 
-		return std::move(sin);
-	}
+	//	return std::move(sin);
+	//}
 
 	//extern __m256d __vdecl_sincos4(__m256d); //The return value is nessecary so that clang does not insert a vzeroupper before the call!
 	//static __inline__ __m256d __DEFAULT_FN_ATTRS _mm256_sincos_pd(__m256d* pcosres, __m256d val)
@@ -98,6 +134,7 @@ __declspec(noinline) void SinCos(Vector4& SinData, Vector4& CosData, Vector4 MyD
 	__m256d Output;
 	//__m256d Output[2];
 
+
 	Input = _mm256_sincos_pd(&Output, Input);
 
 	//_mm256_store_pd(CosData.data(), Output[1]);
@@ -113,6 +150,7 @@ int main()
 	alignas(32) Vector4 SinData;
 	alignas(32) Vector4 CosData;
 	
+
 	SinCos(SinData, CosData, MyData);
 	for (auto i = 0; i < MyData.size(); ++i)
 	{

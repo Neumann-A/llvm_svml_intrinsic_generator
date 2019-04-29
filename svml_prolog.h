8260aa98e585177b19c35ce0c18d12b0257728bd
Alexander Neumann <alexander.neumann@hamburg.de>
2019-04-29 16:11:45 +0200

    small change to prolog

--- svml_prolog.h
+++ svml_prolog.h
@@ -14,7 +14,7 @@
 
 #include <immintrin.h>
 
-#define __SVML_INTRIN_PROLOG static __inline__ 
+#define __SVML_INTRIN_PROLOG [[nodiscard]] static __inline__ 
 #define __DEFAULT_SVML_FN_ATTRS512 __attribute__((__always_inline__, __nodebug__, __target__("avx512"), __min_vector_width__(512)))
 #define __DEFAULT_SVML_FN_ATTRS256 __attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(256)))
 #define __DEFAULT_SVML_FN_ATTRS128 __attribute__((__always_inline__, __nodebug__, __target__("avx"), __min_vector_width__(128)))

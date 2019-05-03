798f8720cade9549366a505becfc3719fde2c95a
Alexander Neumann <alexander.neumann@hamburg.de>
2019-05-03 15:44:00 +0200

    small test progression

--- svml_test_creator.cpp
+++ svml_test_creator.cpp
@@ -6,10 +6,55 @@
 
 #include "svml_test_creator.h"
 
+#include <string>
+#include <iostream>
 namespace svml
 {
-	void write_svml_tests(const svml_mapping_info& info, const fs::path& avxtestfile, const fs::path& avx512testfile)
+	const std::string indent = "    ";
+	std::string  write_test_helper_function(const svml_definition_info& info)
 	{
+		std::string res;
+		
+		res += "__declspec(noinline) void helper_";
+		res += info.strinfo.mmfuncname;
+		res += "(";
+		
+		res += "Vector";
+		res += std::to_string(info.mminfo.PackedElements);
+		res += to_string(packed_type_name_map_info, info.mminfo.Suffix);
+
+		res += ", ";
+		for (auto& param : info.mminfo.ParamList)
+		{
+			if (is_mask_type())
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+		res += ")";
+
 
 	}
+	std::string  write_test(const svml_definition_info& info)
+	{
+		std::string testfunction;
+
+	}
+	void write_svml_tests(const svml_mapping_info& info, const fs::path& avxtestfile, const fs::path& avx512testfile)
+	{
+		for (const auto& elem : info.svml)
+		{
+			if (!elem.mapping_valid)
+			{
+				::std::cerr << "No valid mapping for: " << elem.strinfo.mmfuncname << "! Not generating test"<<"\n";
+				continue;
+			}
+		}
+	}
+
+
 }
